7fd3feae8c5d34866249b56638f10164ebbaa8fa
shellme <shellme9@gmail.com>
2025-10-21 00:52:43 +0900

    fix: Formulaでビルド時にバージョン情報を埋め込むように修正
    - ビルド時にバージョンタグ、コミットハッシュ、ビルド日時を取得
    - -Xフラグでバージョン情報をバイナリに埋め込み
    - Homebrewでインストールした際も正しいバージョン情報を表示

--- Formula/slack-tool.rb
+++ Formula/slack-tool.rb
@@ -1,14 +1,27 @@
 class SlackTool < Formula
   desc "Slackの様々な操作を行うCLIツール"
   homepage "https://github.com/shellme/slack-tool"
-  url "https://github.com/shellme/slack-tool/archive/v0.2.0.tar.gz"
+  url "https://github.com/shellme/slack-tool/archive/v0.2.1.tar.gz"
   sha256 "b5c2f065800fdf4b44a3c2895802d5de1b8eac9238cc322323b724f5e9748943"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-o", "#{bin}/slack-tool", "./cmd/slack-tool"
+    # バージョン情報を取得
+    version_tag = "v#{version}"
+    commit_hash = `git rev-parse --short HEAD`.strip
+    build_date = `date -u '+%Y-%m-%d_%H:%M:%S'`.strip
+    
+    # ビルドフラグを設定
+    ldflags = [
+      "-s -w",
+      "-X github.com/shellme/slack-tool/cmd/slack-tool/cmd.version=#{version_tag}",
+      "-X github.com/shellme/slack-tool/cmd/slack-tool/cmd.commit=#{commit_hash}",
+      "-X github.com/shellme/slack-tool/cmd/slack-tool/cmd.date=#{build_date}"
+    ].join(" ")
+    
+    system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/slack-tool", "./cmd/slack-tool"
   end
 
   test do
